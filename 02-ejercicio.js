420dcb80baae73a4fb372227db2c6057a7e4dd43
YazminSosa <126597464+YazminSosa@users.noreply.github.com>
2023-03-29 17:37:01 -0300

    Add files via upload

---
+++ 02-ejercicio.js
@@ -0,0 +1,19 @@
+//string
+let nombre ="Yazmin"
+//number
+let number = 20;
+//boolean
+true;
+
+//Date
+const fecha_nacimiento = new Date("march 27 2002")
+
+//objeto con propiedades
+const libro = {
+    titulo:"El Principito",
+    autor: "Antoine de Saint-Exupéry",
+    fecha: "6 de abril de 1943",
+    url: "https://www.culturagenial.com/es/libro-el-principito/",
+}
+
+
